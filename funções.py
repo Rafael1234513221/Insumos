a9ccc61a24d72764c707d87fa2bf95330fa06f33
Rafael Sousa <rafinha_estudo17@hotmail.com>
2018-07-20 01:47:29 -0300

    Versão 0.0.1

---
+++ funções.py
@@ -0,0 +1,129 @@
+"""
+Todas as funções necessárias para trabalhar com o banco de dados.
+"""
+
+from tkinter import messagebox
+from time import strftime
+
+
+def arquivo_mensal():
+    """
+    Descobre a data que será o nome do arquivo.txt (mes-ano).
+    """
+    data = strftime('%m-%y')
+    local_arquivo = f"historico/{data}.txt"
+    return str(local_arquivo)
+
+
+def cadastrar_dados(produto, valor):
+    """
+    Escreve no arquivo dados.txt alguma coisa.
+    """
+    data_info = strftime('%D')
+    produto = produto.strip()
+    produto = produto.replace(' ', '_')
+    valor = valor.strip()
+
+    if len(produto) and len(valor) == 0:
+        messagebox.showinfo(message='Ainda existem campos em branco, por favor preencha-os!')
+    
+    elif len(produto) and len(valor) > 0 and valor.isnumeric() == True:
+        with open(f'{arquivo_mensal()}', 'a', encoding='Utf-8') as dados:
+            dados.write(f'{data_info} {produto} {valor}\n')
+
+
+def limpar_dados():
+    """
+    Apaga todos os dados do arquivo externo do mês em andamento.
+    """
+    with open(f'{arquivo_mensal()}', 'w') as dados:
+        dados.write('')
+
+
+def filtrar_dados(tipo: str):
+    """
+    Pega todos os valores que estão no arquivo externo e depois separa de acordo com a entrada.
+    """
+    linhas = []
+    valores = ''
+    with open(f'{arquivo_mensal()}', 'r') as arquivo:
+        for linha in arquivo:
+            linhas.append(linha.strip().split('\n'))
+    for conteúdo in linhas:
+        valores += f'{conteúdo}\n'
+    valores = valores.replace('[', '').replace(']', '').replace("'", '').split()
+    # Filtra por tipo de arquivo.
+    cont_geral = 0
+    lista_datas = []
+    lista_produtos = []
+    lista_preços = []
+    while cont_geral < len(valores)-1:
+        lista_datas.append(str(valores[cont_geral]))
+        lista_produtos.append(str(valores[(cont_geral)+1]))
+        lista_preços.append(float(valores[(cont_geral)+2]))
+        cont_geral += 3
+    if tipo == 'data':
+        return lista_datas 
+    elif tipo == 'produtos':
+        return lista_produtos
+    elif tipo == 'preços':
+        return lista_preços
+    elif tipo == 'todos':
+        return [lista_datas, lista_produtos, lista_preços]
+
+
+def atualiza_salário():
+    """
+    É retornado o valor do seu salário menos as compras feitas.
+    """
+    salário = 1000
+    compras = filtrar_dados('preços')
+    for valor in compras:
+        salário -= valor
+    
+    return f'{salário:.2f}'
+
+
+def compras_mensais():
+    """
+    Localiza o arquivo que diz respeito ao mês de consulta e retorna em uma forma amigável.
+    """
+    extrato_mensal = filtrar_dados('todos')
+    dados_de_saida = f'{"":-^55}\n'
+    dados_de_saida += f'|{"DATA":^10} {"PRODUTO":^30} {"VALOR":^10} |\n'
+    for cont in range(len(extrato_mensal[0])):
+        dados_de_saida += f'|{extrato_mensal[0][cont]:<10} '  # Pega as datas
+        dados_de_saida += f'{extrato_mensal[1][cont]:<40} '   # Pega o nome dos produtos
+        dados_de_saida += f'{extrato_mensal[2][cont]:<10} |'  # Pega o valores dos produtos
+        dados_de_saida += '\n'
+    dados_de_saida += f'{"":-^55}\n'
+    dados_de_saida = dados_de_saida.replace('_', ' ')
+    return dados_de_saida
+
+
+def cria_arquivos():
+    """
+    Cria todos arquivos necessários durante o ano que o usuário digitou as compras.
+    """
+    mes = 1
+    ano = strftime('%y')
+    print(ano)
+    while mes != 12:
+        if mes < 10:
+            str_mes = f'0{mes}'
+            mes += 1
+        if mes >= 10:
+            str_mes = mes
+            mes += 1
+        with open(f'historico/{str_mes}-{ano}.txt', 'w') as arquivo:
+            arquivo.write('')
+
+
+def lança_fatura(vezes):
+    """ 
+    Cadastra nos proximos meses os valores de uma fatura de acordo com o número
+    de parcela que esta tem. Por exemplo se tiver um celular parcelado em 12x
+    essa função lançará nos arquivos dos proxímos 12 meses o valor da parcela.
+    """
+    vezes = vezes.get()
+    print(f'Número de faturas = {vezes}')
---
+++ main.py
@@ -0,0 +1,126 @@
+"""
+"editor.fontFamily": "'Menlo', 'Consolas', 'DejaVu Sans Mono', 'monospace'",
+Interface gráfica do programa.
+"""
+
+from funções import *
+from tkinter import *
+from tkinter import messagebox
+from functools import partial
+
+
+class Aplicativo:
+    def __init__(self, master):
+        self.fonte = ('Monaco', 13)
+        # Criando e escalonando os frames.
+        self.frame0 = Frame(master) 
+        self.frame1 = Frame(master) 
+        self.frame2 = Frame(master) 
+        self.frame3 = Frame(master)
+        self.frame4 = Frame(master) 
+        self.frame0.grid()
+        self.frame1.grid()
+        self.frame2.grid()
+        self.frame3.grid()
+        self.frame4.grid()
+        # Chamandos os elementos gráficos separados por tipagem (botões, entradas, salário).
+        self.entradas()
+        self.botoes()
+        self.mostrar_salário()
+
+    def entradas(self):
+        """
+        Cria todos as caixas de input para recolhimento de dados passados pelo usuário.
+        """
+        # Cria uma caixa de entrada para usuário digitar o nome do produto.
+        Label(self.frame0, text='CADASTRE SUAS COMPRAS', height=2, font=self.fonte).grid(row=0, column=1)
+        Label(self.frame1, text='PRODUTO', font=('Arial', 10), bg='gray', width=8).grid(row=0, column=1)
+        self.ent_produto = Entry(self.frame1, width=20, font=self.fonte)
+        self.ent_produto.focus_force()
+        self.ent_produto.grid(row=0, column=2)
+        # Cria uma caixa de entrada para usuário digitar o valor(R$) do produto.
+        Label(self.frame1, text='VALOR: R$', font=('Arial', 10), bg='gray', width=8).grid(row=1, column=1)
+        self.ent_valor = Entry(self.frame1, width=20, font=self.fonte)
+        self.ent_valor.grid(row=1, column=2)
+
+    def parcela_entrada(self):
+        Label(self.frame1, text='Vezes', font=('Arial', 10), bg='gray', width=8).grid(row=2, column=1)
+        self.entrada_das_parcelas = Entry(self.frame1, width=20, font=self.fonte)
+        self.entrada_das_parcelas.grid(row=2, column=2)
+        self.parcelado['text'] = 'CadastrarParcelas'
+        self.parcelado['command'] = partial(lança_fatura, self.entrada_das_parcelas)
+
+    def botoes(self):
+        """
+        Cria todos os botões e atribui a eles seus devidos comandos.
+        """
+        # Cria o botão de limpar os dados digitados na tela caso haja algum erro de digitação.
+        limpar = Button(self.frame1, text='LIMPAR', bg='red', fg='white', width=11)
+        limpar.grid(row=3, column=1, columnspan=2)
+        limpar['command'] = self.limpar
+        # Cria o botão com função de cadastrar os dados digitados nas entradas pelo usuário.
+        cadastrar = Button(self.frame1, text='CADASTRAR', bg='green', fg='white', width=11)
+        cadastrar['command'] = partial(self.cadastramento, self.ent_produto, self.ent_valor)
+        cadastrar.grid(row=4, column=1, columnspan=2)
+        # Botão referente a verificar o histórico de compras feita no mês em que foi clicado.
+        self.ver_compras = Button(self.frame1, text='EXTRATO', bg='orange', fg='white', width=11)
+        self.ver_compras['command'] = self.vver_compras
+        self.ver_compras.grid(row=5, column=1, columnspan=2)
+        # Botão que diz respeito a comprar parcelado.
+        self.parcelado = Button(self.frame1, text='PARCELADO', bg='blue', fg='white', width=11)
+        self.parcelado['command'] = self.parcela_entrada
+        self.parcelado.grid(row=6, column=1, columnspan=2)
+        'Futuro'
+        # window.bind('<Return>', partial(cadastramento, 'Sumiu', self.ent_produto, self.ent_valor))
+
+    def limpar(self):
+        """
+        Função responsável por limpar os dados que estão na tela.
+        """
+        self.ent_produto.delete(0, END)  # Deleta o que foi digitado na entrada: produto.
+        self.ent_valor.delete(0, END)  # Deleta o que foi digitado na entrada: valor.
+        self.entrada_das_parcelas.delete(0, END)  # Deleta o que foi digitado na entrada: parcelas
+        self.ent_produto.focus_force()  # Faz o foco(cursor) voltar pra caixa de entrada do produto.
+
+    
+    def cadastramento(self, produto, valor):
+        """
+        Pega os dois valores passado pelo usuário e chama uma função externa para cadastra-los.
+        """
+        produto = produto.get()  # Pega os dados que foram digitados pelo usuário e salva numa variável.
+        valor = valor.get()  # Pega os dados que foram digitados pelo usuário e salva numa variável.
+        cadastrar_dados(produto, valor)  # Chama a função externa e passa as duas entradas(produto, valor).
+        self.salario['text'] = f'Salário: R${atualiza_salário()}'  # Atualiza o Label que mostra o salário.
+        self.limpar()  # Chama a função responsável por limpar os campos digitados pelo usuário
+        compras_mensais()
+
+    def mostrar_salário(self):
+        """
+        É retornado o valor do seu salário menos as compras feitas, usando função  externa.
+        """
+        self.salario = Label(self.frame1, text=f'Salário: R${atualiza_salário()}', height=2, font=self.fonte)
+        self.salario.grid(row=7, column=1, columnspan=2)
+
+    def vver_compras(self):
+        """
+        Chama uma função externa que consulta as últimas compras (Mês em questão).
+        """
+        
+        if self.ver_compras['text'] == 'Minimizar extrato':
+            window.geometry('274x226+104+104')
+            self.ver_compras['text'] == 'EXTRATO'
+            self.entradas()
+            self.botoes()
+            self.mostrar_salário()
+
+        elif self.ver_compras['text'] == 'EXTRATO':
+            window.geometry('274x500+104+104')
+            self.ver_compras['text'] = 'Minimizar extrato'
+            self.ver_extrato = Label(self.frame3, text=f'EXTRATO\n{compras_mensais()}')
+            self.ver_extrato.grid(row=8, column=1, columnspan=2)
+
+
+window = Tk()
+window.title('INSUMOS')
+Aplicativo(window)
+window.mainloop()
